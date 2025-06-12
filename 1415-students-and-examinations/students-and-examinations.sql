27db9dbf6ed456d1ea1ff4601cbebb6ac2642fe7
veeraa88 <veeramanitech123@gmail.com>
2025-06-12 21:57:43 +0530

    Time: 1293 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,8 @@
+
+select s.student_id ,s.student_name ,sub.subject_name ,count(e.subject_name ) as attended_exams 
+from Students as s
+cross join Subjects as sub
+left join Examinations as e
+on s.student_id=e.student_id 
+and sub.subject_name=e.subject_name
+group by s.student_id ,s.student_name ,sub.subject_name
